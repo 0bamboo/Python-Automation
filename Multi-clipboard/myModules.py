7d82def974b85de022ad7a0fbaccbbf6e0d3d242
nowl01 <itsabdennacerama@gmail.com>
2022-03-19 06:48:26 +0100

    feat: multi-clipboard

--- Multi-clipboard/myModules.py
+++
@@ -1,5 +0,0 @@
-import sys
-import json
-# if u using ubuntu u need to install xclip first and don't care about the warning underline.
-import pyperclip # i think this module is the same as clipboard
-import clipboard
